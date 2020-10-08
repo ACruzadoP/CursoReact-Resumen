9cbfc296334a0ffcda55ac0c2a0732e3e0e96376
Alberto <albertocruzadopleite@gmail.com>
2020-10-08 23:21:12 +0200

    Modified the required files for the first exercise to work all fine. The file called App.js will be the Parent component on this case

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import FirstChild from "./components/FirstChild"
+
+function App () {
+    return (
+        <div>
+            <FirstChild />
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/FirstChild.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function FirstChild () {
+    return (
+        <div>
+            <h1>This is the first child</h1>
+        </div>
+    )
+}
+
+export default FirstChild
